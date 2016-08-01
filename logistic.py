9d93a57036027d52eaa2dca53b6eb4b066315875
Owen Diehl <ow.diehl@gmail.com>
2016-08-01 17:27:54 -0500

    complete logistic regression

--- logistic.py
+++ logistic.py
@@ -8,25 +8,32 @@ def predict_classes(input):
   return np.round(input)
 
 def cost_fn(hyp, y):
-  return -y * np.log(hyp) - (1-y) * np.log(1 - hyp)
+  return (-y * np.log(hyp) - (1-y) * np.log(1 - hyp)).mean()
 
 def apply_gradient_descent(theta, alpha, x, y):
   hyp = hypothesis(x, theta)
   cost = cost_fn(hyp, y)
 
-  # print alpha / len(hyp) * np.sum(np.transpose(x) * np.array(cost))
-  print alpha / len(hyp) * np.sum(np.transpose(x) * cost)
-  # print type(cost)
+  # These are the same
+  # print (hyp - y).dot(x)
+  # print np.sum((hyp - y) * x.T, 1)
 
-  updated_theta = theta - alpha / len(hyp) * np.sum(cost) * x
-  return (updated_theta, np.sum(cost))
+  updated_theta = theta - alpha * (hyp - y).dot(x)
 
-def run(x, y, iterations=100, alpha=0.1):
+  return (updated_theta, cost)
+
+def run(x, y, max_iterations=10000000, alpha=0.001, converge=0.001, report_every=100):
   x_with_bias = np.pad(np.array(x), ((0,0),(1,0)), mode='constant', constant_values=1)
   y = np.array(y)
   theta = np.zeros(x_with_bias.shape[1])
 
-  # for x in xrange(0, iterations):
-  #   theta, cost = apply_gradient_descent(theta, alpha, x_with_bias, y)
-  #   print 'iteration {}, cost {}'.format(x+1, cost)
+  for x in xrange(0, max_iterations):
+    theta, cost = apply_gradient_descent(theta, alpha, x_with_bias, y)
+
+    if cost < converge:
+      print 'Convergence hit! Iteration #{}, cost: {}, theta: {}'.format(x+1, cost, theta)
+      return (theta, cost)
+
+    if x % report_every == 0:
+      print 'iteration #{}, cost {}, theta {}'.format(x+1, cost, theta)
 
