0e9e3c09d88e92ff6fed9cd820b06022e06d05fb
Owen Diehl <ow.diehl@gmail.com>
2016-08-03 11:31:45 -0500

    linear regression data cleaning

--- linear_regression/linear_regression_test.py
+++ linear_regression/linear_regression_test.py
@@ -4,9 +4,17 @@ import pandas as pd
 raw = pd.read_excel(('./data/kuiper.xls'))
 y = np.array(raw.loc[:, 'Price'])
 
-x1 = raw.loc[:, ['Mileage', 'Cylinder', 'Liter', 'Doors', 'Cruise', 'Sound', 'Leather']]
+numeric_fields = ['Mileage', 'Cylinder', 'Liter']
+boolean_fields = raw.loc[:, ['Cruise', 'Sound', 'Leather']]
+almost_boolean =  ['Doors']
 category_fields = ['Make', 'Model', 'Type']
 # use t1.join(t2) when you get all the other columns ready
+
+def ensure_df(x):
+  if type(x) == pd.DataFrame:
+    return x;
+  else:
+    return pd.DataFrame(x)
 
 def fill_vals(item, base):
   base[item] = 1
@@ -18,14 +26,34 @@ def initialize_values(fields):
     res[val] = 0
   return res
 
+def join_cols(*args):
+  res = []
+  for arg in args:
+    if type(arg) is list:
+      res.extend(arg)
+    else:
+      res.append(arg)
+
+  return reduce(lambda accum, val: accum.join(val), map(ensure_df ,res))
+
 
 category_columns = []
+# map category columns
 for idx, category in enumerate(category_fields):
   options = raw[category].unique()
   new_cols = raw[category].apply(lambda x: pd.Series(fill_vals(x, initialize_values(options))))
   category_columns.append(new_cols)
 
-final_x = x1.join(category_columns)
+# map doors to boolean
+doors_col = raw.loc[:, 'Doors'].apply(lambda x: int(x == 4))
 
-print final_x.head(), final_x.shape
+numeric_cols = []
+# map numeric columns to feature-scaled versions
+for idx, numeric_col in enumerate(numeric_fields):
+  base_col = raw.loc[:, numeric_col]
+  std = base_col.std()
+  mean = base_col.mean()
+  scaled = base_col.apply(lambda x: (x - mean)/std)
+  numeric_cols.append(scaled)
 
+final_x = join_cols(category_columns, doors_col, numeric_cols, boolean_fields)
