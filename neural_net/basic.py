587067812b02e757825597c723e6a3ddfbd4d788
Owen Diehl <ow.diehl@gmail.com>
2016-08-06 19:44:17 -0500

    refactors to class

--- neural_net/basic.py
+++ neural_net/basic.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from scipy.special import expit
 
 #----------------- hyper params
 # input units - nx1 vector
@@ -11,30 +12,73 @@ import math
 # model params
 # _, num_classes = y.shape
 # m, num_inputs = x.shape
+def sigmoid(input, deriv=False):
+  input = expit(input)
+  if deriv == True:
+    return np.multiply(input, 1-input)
+  else:
+    return input
 
-def build_theta(num_features, num_classes, hidden_length, num_layers):
-  weights = []
-  # 1 step input->hidden
-  # hidden_length-1 steps hidden->hidden
-  # (for steps where destination is hidden, include a bias unit)
-  # 1 step hidden->output
-  for x in range(0, num_layers+1):
-    # first
-    if x == 0:
-      shape = (hidden_length, num_features)
-    # last
-    elif x == num_layers:
-      shape = (num_classes, hidden_length+1)
-    # hidden->hidden (all others)
-    else :
-      shape = (hidden_length, hidden_length+1)
+def back_prop(h_x, y, weights):
+  d_neg_1 = h_x - y
+  d_neg_2 = np.dot(d_neg_1, theta_2) * (L2 * (1 - L2))
 
-    this_weight = np.random.randn(*shape)
-    weights.append(this_weight)
-  return weights
 
-def run(x, y, num_layers, hidden_length, epochs=1):
-  theta = build_theta(len(x), y.shape[1], hidden_length, num_layers)
+class Net:
+  def __init__(self, x, y, num_layers=2, hidden_length=2, default_bias=0.01, activation=sigmoid):
+    self.x = x
+    self.y = y
+    self.m = len(x)
+    self.num_features = x.shape[1]
+    self.num_classes = y.shape[1]
+    self.activation = activation
+    self.num_layers = num_layers
+    self.hidden_length = hidden_length
+
+    self.set_weights(self.build_theta(self.num_features, self.num_classes, self.hidden_length, self.num_layers))
+
+  def get_weights(self,):
+    return self.weights
+
+  def set_weights(self, weights):
+    self.weights = weights
+
+  def build_theta(self, num_features, num_classes, hidden_length, num_layers):
+    weights = []
+    # 1 step input->hidden
+    # hidden_length-1 steps hidden->hidden
+    # (for steps where destination is hidden, include a bias unit)
+    # 1 step hidden->output
+    for x in range(0, num_layers+1):
+      # first
+      if x == 0:
+        shape = (num_features+1, hidden_length)
+      # last
+      elif x == num_layers:
+        shape = (hidden_length, num_classes)
+      # hidden->hidden (all others)
+      else :
+        shape = (hidden_length+1, hidden_length)
+
+      this_weight = np.random.randn(*shape)*0.01
+      weights.append(this_weight)
+    return weights
+
+  def forward_pass(self, input, theta, bias=True):
+    if bias == True:
+      return np.pad(np.array(input), ((0,0),(1,0)), mode='constant', constant_values=1).dot(theta)
+    else:
+      return input.dot(theta)
+
+  def predict(self):
+    res = self.x
+    last_idx = len(self.weights) - 1
+    for idx, theta in enumerate(self.weights):
+      if idx == last_idx:
+        res = self.forward_pass(res, theta, bias=False)
+      else:
+        res = self.forward_pass(res, theta, bias=True)
+    return res
 
 # for i = 1; i = m; -++:
 #   forward prop(xi, yi) -> get activations (a)
