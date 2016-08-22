2ab02a9227dee908870da0d1a44191eec97933f0
Owen Diehl <ow.diehl@gmail.com>
2016-08-22 13:59:12 -0400

    adds entry for generating multiple classifier outputs from string output fields

--- neural_net/basic_test.py
+++ neural_net/basic_test.py
@@ -2,15 +2,17 @@ import numpy as np
 import basic as basic
 import pandas as pd
 from sklearn.datasets import fetch_mldata
-# load x
-# load y
-# wrap in np arrays
-#
-# csv_input = pd.read_csv('../logistic_regression/iris/Iris.csv')
 
+
+# csv_input = pd.read_csv('../logistic_regression/iris/Iris.csv')
 # x = np.array( csv_input.loc[:, 'SepalLengthCm':'PetalWidthCm'])
-# x2 = np.array(csv_input.loc[:, 'SepalLengthCm':'SepalWidthCm'])
 # y = np.atleast_2d(np.array(csv_input['Species'].map(lambda x: int(x == 'Iris-setosa'))))
+# x2 = np.array(csv_input.loc[:, 'SepalLengthCm':'SepalWidthCm'])
+
+# y_cols = []
+# for entry in csv_input['Species'].unique():
+#   y_cols.append(csv_input['Species'].map(lambda x: int(x == entry)))
+# y = np.array(y_cols).T
 
 num_sets = 10000
 mnist = fetch_mldata('MNIST original')
