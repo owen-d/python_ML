fixes issue with multiclass example. still need to implement some sort of softmax

neural_net/basic_test.py
@@ -14,16 +14,16 @@ from sklearn.datasets import fetch_mldata
 #   y_cols.append(csv_input['Species'].map(lambda x: int(x == entry)))
 # y = np.array(y_cols).T
 
-num_sets = 40000
+num_sets = 70000
 mnist = fetch_mldata('MNIST original')
 x = mnist.data[:num_sets]
-y = mnist.target[:num_sets]
-y = np.reshape(y, (y.shape[0], 1))
+y = map(lambda x: int(x == 0), mnist.target[:num_sets])
+y = np.reshape(y, (num_sets, 1))
 # y = np.atleast_2d(mnist.target).T[:num_sets]
 
 n = basic.Net(x,y, num_layers=2, hidden_length=8)
 regularization_term = 0
-alpha = 0.005
+alpha = 0.01
 
-n.run(alpha=alpha, report_every=2)
+n.run(alpha=alpha, report_every=5)
 print n.get_weights()